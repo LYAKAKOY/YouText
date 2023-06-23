539924fd5e43901c8e19ce807d90d0c06352481f
Lewis <LYAKAKOY@yandex.ru>
2023-06-23 19:36:03 +0300

    Создание приложения для генератора статьи

--- web_app/config/settings.py
+++ web_app/config/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'article_generator.apps.ArticleGeneratorConfig',
 ]
 
 MIDDLEWARE = [
