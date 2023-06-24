c421e3f65d2dd978c335128ab511aee9f2f09f32
Lewis <LYAKAKOY@yandex.ru>
2023-06-24 03:27:31 +0300

    Добавление обрезки видео и аудио файлов

--- web_app/article_generator/downloading_youtube_videos.py
+++ web_app/article_generator/downloading_youtube_videos.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+from pydub import AudioSegment
 import youtube_dl
+from moviepy.video.io.VideoFileClip import VideoFileClip
 
 
 def download_video(video_url: str) -> None:
@@ -19,3 +22,36 @@ def download_audio(video_url: str) -> None:
     }
     ydl = youtube_dl.YoutubeDL(audio_options)
     ydl.download([video_url])
+
+
+def video_cropping(start_time: str, end_time: str) -> None:
+    video_path = 'web_app/video/youtube_video.mp4'
+    video = VideoFileClip(video_path)
+
+    start = datetime.strptime(start_time, "%H:%M:%S")
+    end = datetime.strptime(end_time, "%H:%M:%S")
+
+    # Преобразуем время в секунды
+    start_seconds = start.hour * 3600 + start.minute * 60 + start.second
+    end_seconds = end.hour * 3600 + end.minute * 60 + end.second
+
+    # Обрезаем видео
+    cropped_video = video.subclip(start_seconds, end_seconds)
+    cropped_video.write_videofile(video_path)
+
+
+def audio_cropping(start_time: str, end_time: str) -> None:
+    # Загружаем аудиофайл
+    audio_path = 'web_app/audio/youtube_audio.mp3'
+    audio = AudioSegment.from_file(audio_path)
+
+    # Преобразуем время начала и время окончания в миллисекунды
+    start = datetime.strptime(start_time, "%H:%M:%S")
+    end = datetime.strptime(end_time, "%H:%M:%S")
+
+    start_ms = (start.hour * 3600 + start.minute * 60 + start.second) * 1000
+    end_ms = (end.hour * 3600 + end.minute * 60 + end.second) * 1000
+
+    cropped_audio = audio[start_ms:end_ms]
+
+    cropped_audio.export(audio_path, format='mp3')
--- web_app/article_generator/speech_recognition.py
+++ web_app/article_generator/speech_recognition.py
@@ -6,4 +6,7 @@ model = whisper.load_model("base")
 def speech_recognition_base():
     global model
     result = model.transcribe("./audio/youtube_audio.mp3", fp16=False)
-    return result["text"]
+    text = ''
+    for segment in result['segments']:
+        text += f"[{round(segment['start'], 2)}:{round(segment['end'], 2)}] {segment['text']}"
+    return text
